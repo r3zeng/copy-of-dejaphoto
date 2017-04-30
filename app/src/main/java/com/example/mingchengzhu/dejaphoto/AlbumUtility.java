e005d050baa640c9c7d60e984d8c06af1c34b8de
Andres Meza <anmeza@ucsd.edu>
2017-04-30 15:53:59 -0700

    Albert|Andy: Implemented more methods for AlbumUtility

--- app/src/main/java/com/example/mingchengzhu/dejaphoto/AlbumUtility.java
+++ app/src/main/java/com/example/mingchengzhu/dejaphoto/AlbumUtility.java
@@ -1,14 +1,21 @@
 package com.example.mingchengzhu.dejaphoto;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
+import java.nio.channels.FileChannel;
 
 /**
  * Created by andymeza on 4/29/17.
@@ -18,6 +25,12 @@ public class AlbumUtility {
 
     private final static String path = Environment.getExternalStorageDirectory().toString();
     private final static String filePath = "/dejaphoto/media/app images/";
+
+    public static void createAlbum()
+    {
+        File dir = new File(path, filePath);
+        dir.mkdirs();
+    }
 
     public static boolean dejaAlbumExists()
     {
@@ -40,35 +53,55 @@ public class AlbumUtility {
         return albumDir().listFiles().length;
     }
 
-  /*  public static void insertPhoto(Uri image)
+    public static void insertPhoto(Uri image, Context context) throws IOException
     {
-        String photoPath = image.getPath();
 
         String res = null;
         String[] proj = { MediaStore.Images.Media.DATA };
-        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        Cursor cursor = context.getContentResolver().query(image, proj, null, null, null);
         if(cursor.moveToFirst()){;
             int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             res = cursor.getString(column_index);
         }
         cursor.close();
 
+        File sourceFile = new File(res);
+        File destFile = new File(albumDir(), java.util.UUID.randomUUID().toString());
+        if (!sourceFile.exists()) {
+            return;
+        }
 
+        FileChannel source = null;
+        FileChannel destination = null;
+        source = new FileInputStream(sourceFile).getChannel();
+        destination = new FileOutputStream(destFile).getChannel();
+        if (destination != null && source != null) {
+            destination.transferFrom(source, 0, source.size());
+        }
+        if (source != null) {
+            source.close();
+        }
+        if (destination != null) {
+            destination.close();
+        }
 
-        //scan the image so show up in album
-        MediaScannerConnection.scanFile(this,
-                new String[] { photoPath }, null,
-                new MediaScannerConnection.OnScanCompletedListener() {
-                    public void onScanCompleted(String path, Uri uri) {
-                        if(Bitmap.Config.LOG_DEBUG_ENABLED) {
-                            Log.d(Config.LOGTAG, "scanned : " + path);
-                        }
-                    }
-                });
+    }
 
+    public static boolean releasePhoto(String removePhoto) throws IOException
+    {
+        File photoPath = new File(removePhoto);
 
-    } */
+        return photoPath.delete();
+    }
 
+    public static File[] returnPhotoList()
+    {
+        return albumDir().listFiles();
+    }
 
 
 }
+
+
+
+
