a556657b56e2b65a14cf6f5b08de47f4c957decc
Albert Christensen <aschrist@ucsd.edu>
2017-05-12 15:44:04 -0700

    add FetchIntentService and Constants

---
+++ app/src/main/java/com/example/mingchengzhu/dejaphoto/Constants.java
@@ -0,0 +1,17 @@
+package com.example.mingchengzhu.dejaphoto;
+
+/**
+ * Created by sterling on 5/12/17.
+ */
+
+public final class Constants {
+    public static final int FETCH_ADDRESS_SUCCESS = 0;
+    public static final int FETCH_ADDRESS_FAILURE = 1;
+    public static final String PACKAGE_NAME =
+            "com.google.android.gms.location.sample.locationaddress";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME +
+            ".RESULT_DATA_KEY";
+    public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME +
+            ".LOCATION_DATA_EXTRA";
+}
---
+++ app/src/main/java/com/example/mingchengzhu/dejaphoto/FetchAddressIntentService.java
@@ -0,0 +1,93 @@
+package com.example.mingchengzhu.dejaphoto;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class FetchAddressIntentService extends IntentService {
+    protected Location mLastLocation;
+    protected ResultReceiver mReceiver;
+
+    private static final String TAG = "FetchAddress";
+
+    public FetchAddressIntentService() {
+        super("FetchAddressIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String errorMessage = "";
+
+        // Get the location passed to this service through an extra.
+        Location location = intent.getParcelableExtra(
+                Constants.LOCATION_DATA_EXTRA);
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        List<Address> addresses = null;
+
+        try {
+            addresses = geocoder.getFromLocation(
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    // In this sample, get just a single address.
+                    1);
+        } catch (IOException ioException) {
+            // Catch network or other I/O problems.
+            Log.e(TAG, "service not available", ioException);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            // Catch invalid latitude or longitude values.
+            errorMessage = "Invalid coordinates.";
+            Log.e(TAG, errorMessage + ". " +
+                    "Latitude = " + location.getLatitude() +
+                    ", Longitude = " +
+                    location.getLongitude(), illegalArgumentException);
+        }
+
+        // Handle case where no address was found.
+        if (addresses == null || addresses.size()  == 0) {
+            if (errorMessage.isEmpty()) {
+                errorMessage = "no address found";
+                Log.e(TAG, errorMessage);
+            }
+            deliverResultToReceiver(Constants.FETCH_ADDRESS_FAILURE, errorMessage);
+        } else {
+            Address address = addresses.get(0);
+            ArrayList<String> addressFragments = new ArrayList<String>();
+
+            // Fetch the address lines using getAddressLine,
+            // join them, and send them to the thread.
+            for(int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                addressFragments.add(address.getAddressLine(i));
+            }
+            Log.i(TAG, "Found the address");
+            deliverResultToReceiver(Constants.FETCH_ADDRESS_SUCCESS,
+                    TextUtils.join(System.getProperty("line.separator"),
+                            addressFragments));
+        }
+    }
+
+    private void deliverResultToReceiver(int resultCode, String message) {
+        Bundle bundle = new Bundle();
+        bundle.putString(Constants.RESULT_DATA_KEY, message);
+        mReceiver.send(resultCode, bundle);
+    }
+}
