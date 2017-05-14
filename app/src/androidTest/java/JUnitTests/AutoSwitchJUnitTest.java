2879d85f4cae7eca15ab8e4634504b1bf9a23b61
Mingcheng Zhu <zhumc11@gmail.com>
2017-05-14 11:50:02 -0700

    Mingcheng | Scott, G30-16 auto-switch Junit Test

--- app/src/androidTest/java/JUnitTests/AutoSwitchJUnitTest.java
+++ app/src/androidTest/java/JUnitTests/AutoSwitchJUnitTest.java
@@ -1,9 +1,33 @@
 package JUnitTests;
+
+import com.example.mingchengzhu.dejaphoto.AutoSwitch;
 import com.example.mingchengzhu.dejaphoto.Tracker;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 /**
  * Created by mingchengzhu on 5/14/17.
  */
 
 public class AutoSwitchJUnitTest {
+
+    int time;
+    AutoSwitch autoSwitch;
+
+    @Before
+    public void setUp(){
+        autoSwitch = new AutoSwitch(null, null, null, 0);
+        time = 10;
+    }
+
+    @Test
+    public void testTimeSetter(){
+
+        autoSwitch.setTime(time);
+        assertEquals(time, autoSwitch.getTime());
+    }
+
 }
