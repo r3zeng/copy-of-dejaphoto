544a65d3dba93b65c80f3732fd387d96039ae83a
Albert Christensen <aschrist@ucsd.edu>
2017-05-14 10:56:59 -0700

    add some logging to AutoSwitch

--- app/src/main/java/com/example/mingchengzhu/dejaphoto/AutoSwitch.java
+++ app/src/main/java/com/example/mingchengzhu/dejaphoto/AutoSwitch.java
@@ -1,6 +1,7 @@
 package com.example.mingchengzhu.dejaphoto;
 
 import android.os.Handler;
+import android.util.Log;
 
 /**
  * Created by Tianhui Cao, Mingcheng Zhu on 5/10/17.
@@ -8,6 +9,8 @@ import android.os.Handler;
  */
 
 public class AutoSwitch implements Runnable {
+    private static final String TAG = "AutoSwitch";
+
     Handler handler;
     MainActivity activity;
     private int refresh_time;
@@ -21,9 +24,11 @@ public class AutoSwitch implements Runnable {
 
     @Override
     public void run() {
+        Log.i(TAG, "Auto switch timer firing now");
+
         //call the change background method here
-        PhotoManager algo = new PhotoManager(activity);
-        algo.next();
+        PhotoManager manager = new PhotoManager(activity);
+        manager.next();
 
         handler.postDelayed(this, refresh_time);//3 minutes 180000
     }
