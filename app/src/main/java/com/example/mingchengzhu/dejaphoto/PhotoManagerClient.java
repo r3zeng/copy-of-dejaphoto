197e3f0d2e558f68f6264de6317c9c34c57ce990
Albert Christensen <aschrist@ucsd.edu>
2017-05-14 12:49:04 -0700

    G30-38 add more comments PhotoManagerClient

--- app/src/main/java/com/example/mingchengzhu/dejaphoto/PhotoManagerClient.java
+++ app/src/main/java/com/example/mingchengzhu/dejaphoto/PhotoManagerClient.java
@@ -4,14 +4,26 @@ package com.example.mingchengzhu.dejaphoto;
  * Created by sterling on 5/14/17.
  */
 
+/**
+ * Interface to be implemented by classes that use PhotoManager
+ */
 public interface PhotoManagerClient {
 
+    /**
+     * The most recent swipe direction
+     */
     public enum SwipeDirection {
         right, left, neither
     }
     public SwipeDirection lastSwipe = SwipeDirection.neither;
 
+    /**
+     * A composed Tracker object for information on the user's current time and location
+     */
     public Tracker tracker = new Tracker();
 
+    /**
+     * A callback method to inform the client that the currentPhoto field has changed
+     */
     public void currentPhotoChanged();
 }
