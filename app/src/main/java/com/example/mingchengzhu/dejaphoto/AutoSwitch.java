b987c2498b3b81bda2f20e46e76b9ed9f1e5a82d
Tianhui Cao <t9cao@ucsd.edu>
2017-05-14 14:44:23 -0700

    Scott|Mingcheng: Add method header to AutoSwitch

--- app/src/main/java/com/example/mingchengzhu/dejaphoto/AutoSwitch.java
+++ app/src/main/java/com/example/mingchengzhu/dejaphoto/AutoSwitch.java
@@ -10,12 +10,19 @@ import android.util.Log;
 public class AutoSwitch implements Runnable {
     private static final String TAG = "AutoSwitch";
 
+    /* Field */
     Handler handler;
     PhotoManagerClient client;
     PhotoManager manager;
     private int refresh_time;
 
-
+    /**
+     * Constructor
+     * @param client PhotoManagerClient object
+     * @param manager PhotoManager object
+     * @param handler Handler object
+     * @param refresh_time duration to refresh the timer
+     */
     public AutoSwitch(PhotoManagerClient client, PhotoManager manager, Handler handler, int refresh_time){
         this.handler = handler;
         this.client = client;
@@ -27,18 +34,29 @@ public class AutoSwitch implements Runnable {
     public void run() {
         Log.i(TAG, "Auto switch timer firing now");
 
-        //call the change background method here
+        // Call the change background method here
         manager.next();
 
         handler.postDelayed(this, refresh_time);//3 minutes 180000
     }
 
+    /**
+     * Getter method for refresh_time field
+     * @return refresh_time
+     */
     public int getTime(){return refresh_time;}
 
+    /**
+     * Setter method for refresh_time field
+     * @param time the updated new time
+     */
     public void setTime(int time){
         refresh_time = time;
     }
 
+    /**
+     * Refresh the time
+     */
     public void refresh(){
         Log.i(TAG, "The timer is refresed");
         if (this != null) {
