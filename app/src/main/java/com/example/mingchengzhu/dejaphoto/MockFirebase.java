c70ffe518d52904dea9886734d8132168d930502
Albert Christensen <aschrist@ucsd.edu>
2017-06-02 16:13:00 -0700

    Albert|Andy: G30-98 start on MockFirebase for testing

---
+++ app/src/main/java/com/example/mingchengzhu/dejaphoto/MockFirebase.java
@@ -0,0 +1,18 @@
+package com.example.mingchengzhu.dejaphoto;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+/**
+ * Created by sterling on 6/2/17.
+ */
+
+public class MockFirebase implements iFirebase {
+
+    public void uploadDejaPhoto(DejaPhoto photo, OnSuccessListener successListener, OnFailureListener failureListener) {
+    }
+
+    public void downloadDejaPhoto(String pathName, OnSuccessListener successListener, final OnFailureListener failureListener) {
+    }
+
+}
