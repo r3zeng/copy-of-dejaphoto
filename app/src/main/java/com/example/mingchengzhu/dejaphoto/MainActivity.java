a12cd77ea001256d8c58aedf5ca21c699a375162
Mingcheng Zhu <zhumc11@gmail.com>
2017-06-03 16:20:09 -0700

    G30- 40 Mingcheng: debugged set locationName

--- app/src/main/java/com/example/mingchengzhu/dejaphoto/MainActivity.java
+++ app/src/main/java/com/example/mingchengzhu/dejaphoto/MainActivity.java
@@ -850,7 +850,7 @@ public class MainActivity extends AppCompatActivity
             gotLocationText(photo, photo.getLocationName());
         }
 
-        if (location != null) {
+        else if (location != null) {
             if (resultReceiver == null) {
                 resultReceiver = new AddressResultReceiver(new Handler());
             }
