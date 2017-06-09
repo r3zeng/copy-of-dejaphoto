f68b0031e09320b013128c5ba75ef172931c2403
r3zeng <r3zeng@ucsd.edu>
2017-06-08 23:15:31 -0700

    refactor

--- app/src/main/java/com/example/mingchengzhu/dejaphoto/MockFirebase.java
+++ app/src/main/java/com/example/mingchengzhu/dejaphoto/MockFirebase.java
@@ -5,6 +5,8 @@ import android.widget.TextView;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.util.ArrayList;
+
 /**
  * Created by sterling on 6/2/17.
  */
@@ -19,6 +21,31 @@ public class MockFirebase implements iFirebase {
     }
 
     public void downloadDejaPhoto(final String id, OnSuccessListener successListener, final OnFailureListener failureListener) {
+    }
+
+    @Override
+    public void loadFriendsFromDataBase() {
+        
+    }
+
+    @Override
+    public void CheckIfFriend(String DataBaseID) {
+
+    }
+
+    @Override
+    public void StartUserUpdateListener() {
+
+    }
+
+    @Override
+    public void addFriend(String Email) {
+
+    }
+
+    @Override
+    public ArrayList<String> getAllMutalFriend() {
+        return null;
     }
 
     // for karma count
