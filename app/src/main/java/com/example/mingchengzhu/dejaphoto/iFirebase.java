c15a06189a14b25c669ef435f9787982ef643602
Andres Meza <anmeza@UCSD.EDU>
2017-06-09 22:21:55 -0700

    G30-99: headers in interfaces

--- app/src/main/java/com/example/mingchengzhu/dejaphoto/iFirebase.java
+++ app/src/main/java/com/example/mingchengzhu/dejaphoto/iFirebase.java
@@ -17,7 +17,7 @@ public interface iFirebase {
     String getUserID();
 
     /**
-     *
+     * Uploads a dejaphoto to Firebase database
      * @param photo
      * @param successListener
      * @param failureListener
@@ -25,7 +25,7 @@ public interface iFirebase {
     void uploadDejaPhoto(DejaPhoto photo, OnSuccessListener successListener, OnFailureListener failureListener);
 
     /**
-     *
+     * Downloads a dejaphoto with all of its fields
      * @param id
      * @param successListener
      * @param failureListener
@@ -33,47 +33,47 @@ public interface iFirebase {
     void downloadDejaPhoto(final String id, OnSuccessListener successListener, OnFailureListener failureListener);
 
     /**
-     *
+     * Downloads all photos of a user including friends
      */
     void downloadAllPhotos();
 
     /**
-     *
+     * Loads a list of friends from the database for a user
      */
     void loadFriendsFromDataBase();
 
     /**
-     *
+     * Checks if someone is a friend in firebase
      * @param DataBaseID
      */
     void CheckIfFriend(String DataBaseID);
 
     /**
-     *
+     * listener for user changes
      */
     void StartUserUpdateListener();
 
     /**
-     *
+     * adds a friend field to a user on firebase
      * @param Email
      */
     void addFriend(String Email);
 
     /**
-     *
+     * list of mutual friends
      * @return
      */
     ArrayList<String> getAllMutalFriend();
 
     /**
-     *
+     * total karma count among all users
      * @param id
      * @param view
      */
     void displayKCount(final String id, final TextView view);
 
     /**
-     *
+     * displays location name from firebase
      * @param id
      * @param view
      * @param editText
@@ -81,14 +81,14 @@ public interface iFirebase {
     void displayLName(final String id, final TextView view, final EditText editText);
 
     /**
-     *
+     * updates kcount for an image in firebase
      * @param id
      * @param count
      */
     void setKCount(final String id, final long count);
 
     /**
-     *
+     * updates an images location name
      * @param id
      * @param lname
      */
